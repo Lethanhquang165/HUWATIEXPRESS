07f837614f7825027bbf2d15d47f7a134187b5c4
Lê Văn Tiến <l.vantien286@gmail.com>
2017-05-24 06:22:42 +0700

    Init Android Project

--- HUWATIExpress/HUWATIExpress/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace HUWATIExpress.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- HUWATIExpress/HUWATIExpress/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace HUWATIExpress.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
