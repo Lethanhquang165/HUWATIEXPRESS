be0f9086f12e5937b4b314bca10796525691f469
Lê Văn Tiến <l.vantien286@gmail.com>
2017-05-27 11:47:15 +0700

    Create Register Layout

---
+++ app/src/main/java/vn/edu/hutech/lvtien/huwatiexpressapplication/View/Register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.hutech.lvtien.huwatiexpressapplication.View.Register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.edu.hutech.lvtien.huwatiexpressapplication.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
