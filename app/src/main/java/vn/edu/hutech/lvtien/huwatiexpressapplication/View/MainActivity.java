e607f4a00982f35cafea4a2a179a0f2549f0be10
Lê Văn Tiến <l.vantien286@gmail.com>
2017-05-31 05:21:12 +0700

    create ticket layout, update Navigation

--- app/src/main/java/vn/edu/hutech/lvtien/huwatiexpressapplication/View/MainActivity.java
+++ app/src/main/java/vn/edu/hutech/lvtien/huwatiexpressapplication/View/MainActivity.java
@@ -1,15 +1,119 @@
 package vn.edu.hutech.lvtien.huwatiexpressapplication.View;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import vn.edu.hutech.lvtien.huwatiexpressapplication.R;
+import vn.edu.hutech.lvtien.huwatiexpressapplication.View.Fragment.TicketFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.appBarLayout)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.nav_view)
+    NavigationView navView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    private ActionBarDrawerToggle drawerToggle;
+
+    /**
+     * Init Title
+     */
+    private CharSequence _drawerTitle;
+    private CharSequence _title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_open);
+
+        /** Set the drawer toggle as the DrawerListener */
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        navView.setNavigationItemSelectedListener(this);
+
+        navView.setCheckedItem(R.id.nav_ticket);
+
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_ticket:
+                break;
+            case R.id.nav_check_ticket:
+                break;
+            case R.id.nav_schedule:
+                break;
+            case R.id.nav_car_information:
+                break;
+            case R.id.nav_passenger_information:
+                break;
+            case R.id.nav_account_information:
+                break;
+            case R.id.nav_change_password:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_contact_us:
+                break;
+            case R.id.nav_about_us:
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
+
+
