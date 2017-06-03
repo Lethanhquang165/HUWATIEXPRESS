7a57b0bb994257b3496c227589968f980a5a3d5e
Lê Văn Tiến <l.vantien286@gmail.com>
2017-06-03 09:35:11 +0700

    Booking Information Layout

---
+++ app/src/main/java/vn/edu/hutech/lvtien/huwatiexpressapplication/View/Activity/BookingInformationActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.hutech.lvtien.huwatiexpressapplication.View.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.edu.hutech.lvtien.huwatiexpressapplication.R;
+
+public class BookingInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_information);
+    }
+}
