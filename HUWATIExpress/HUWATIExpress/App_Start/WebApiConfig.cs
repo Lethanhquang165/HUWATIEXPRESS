dfea0016398f9eedd908666c6efca1e8761a070c
Leo Andres <lethanhquang165@gmail.com>
2017-06-17 10:58:39 +0700

    Check-in Config file for consistency

--- HUWATIExpress/HUWATIExpress/App_Start/WebApiConfig.cs
+++ HUWATIExpress/HUWATIExpress/App_Start/WebApiConfig.cs
@@ -19,11 +19,24 @@ namespace HUWATIExpress
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional}
+                routeTemplate: "api/{controller}/{action}/{startPoint}",
+                defaults: new {startPoint = RouteParameter.Optional}
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetTripByPoints",
+                routeTemplate: "api/{controller}/{action}/{startPoint}/{defPoint}/{startDate}",
+                defaults: new { startPoint = RouteParameter.Optional, defPoint = RouteParameter.Optional, startDate = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "GetListOfTripHaveEmptySeatByStartDaY",
+                routeTemplate: "api/{controller}/{action}/{startDay}",
+                defaults: new { startDay = RouteParameter.Optional }
             );
         }
     }
---
+++ HUWATIExpress/HUWATIExpress/Controllers/Mobile/TripController.cs
@@ -0,0 +1,185 @@
+﻿using HUWATIExpress.Models;
+using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace HUWATIExpress.Controllers.Mobile
+{
+    public class TripController : ApiController
+    {
+        huwadbDataContext db = new huwadbDataContext();
+        /// <summary>
+        /// GET: LIST TRIP BY START POINT END DEF POINT
+        /// Link: api/gettripbypoints/{startPoint}/{defPoint}/{startDate}
+        /// </summary> 
+        [HttpGet]
+        public IQueryable<TripByPointsListItem> GetTripByPoints(string startPoint, string defPoint, DateTime startDate)
+        {
+            var listItem = from company in db.Companies
+
+                           join bus in db.Bus on company.Company_Id equals bus.Company_Id
+                           join bustype in db.Bus_Types on bus.Bus_Type_Id equals bustype.Bus_Type_Id
+                           join seat in db.Seats on bus.Bus_Id equals seat.Bus_Id
+                           join trip in db.Trips on bus.Bus_Id equals trip.Bus_Id
+                           join route in db.Routes on trip.Route_Id equals route.Route_Id
+
+                           where route.Start_Point == startPoint && route.Def_Point == defPoint && trip.Start_Date == startDate
+
+                           select new TripByPointsListItem
+                           {
+                               CompanyName = company.Company_Name,
+                               Bus_Type_Name = bustype.Bus_Type_Name,
+                               Start_Point = route.Start_Point,
+                               Def_Point = route.Def_Point,
+                               Start_Date = trip.Start_Date,
+                               Start_Time = trip.Start_Time,
+                               Seat_Code = seat.Seat_Code,
+                               Seat_Position = seat.Seat_Position,
+                               Price = trip.Price
+                           };
+            return listItem;
+        }
+
+        /// <summary>
+        /// GET: LIST TRIP HAVE EMPTY SEAT
+        /// Link: api/getlistoftriphavemptyseat
+        /// </summary> 
+        [HttpGet]
+        public IQueryable<TripsHaveEmptySeatListItem> GetListOfTripHaveEmptySeat()
+        {
+            ///<summary>
+            ///Select	s.Seat_Id, t.Trip_Id
+            ///From Bus b, Seat s, Trip t
+            ///Where    b.Bus_Id = s.Bus_Id and
+            ///         t.Bus_Id = b.Bus_Id and
+            ///         s.Status = 1
+            ///         EXCEPT
+            ///         Select sm.Seat_Id, t.Trip_Id
+            ///         From Trip t, Seat_Map sm
+            ///         Where t.Trip_Id = sm.Trip_Id and sm.Status = 1
+            ///	)       as subItem1st
+            ///</summary>
+
+            var subItem1st = from seat in db.Seats
+
+                             join bus in db.Bus on seat.Bus_Id equals bus.Bus_Id
+                             join trip in db.Trips on bus.Bus_Id equals trip.Bus_Id
+
+                             select new AvailableSeatByTripListItem
+                             {
+                                 Seat_Id = seat.Seat_Id,
+                                 Trip_Id = trip.Trip_Id,
+                                 Bus_Id = bus.Bus_Id,
+                             };
+
+            ///<summary>
+            ///Select Count(s.Seat_Id) as [Seat Trống], s.Trip_Id
+            ///From subItem1 
+            ///Group By s.Trip_Id as subItem2nd
+            ///</summary>
+            var subItem2nd = from items in subItem1st
+                             group items by items.Trip_Id into g
+                             select new { g.Key, g };
+
+            ///<summary>
+            ///
+            ///</summary>
+
+            var listItem = from trip in db.Trips
+
+                           join bus in db.Bus on trip.Bus_Id equals bus.Bus_Id
+                           join company in db.Companies on bus.Company_Id equals company.Company_Id
+                           join busType in db.Bus_Types on bus.Bus_Type_Id equals busType.Bus_Type_Id
+                           join route in db.Routes on trip.Route_Id equals route.Route_Id
+                           join items in subItem2nd on trip.Trip_Id equals items.Key
+
+                           select new TripsHaveEmptySeatListItem
+                           {
+                               Trip_Id = trip.Trip_Id,
+                               Bus_Id = bus.Bus_Id,
+                               Bus_Type_Name = busType.Bus_Type_Name,
+                               CompanyName = company.Company_Name,
+                               Start_Point = route.Start_Point,
+                               Def_Point = route.Def_Point,
+                               Start_Date = trip.Start_Date,
+                               Start_Time = trip.Start_Time,
+                               Price = trip.Price,
+                               Empty_Seat_Sum = items.g.Select(x => x.Seat_Id).Count()
+                           };
+
+            return listItem;
+        }
+
+        ///<summary>
+        /// GET: LIST TRIP HAVE EMPTY SEAT BY START_DATE
+        /// Link: api/getlistoftriphavemptyseatbystartdate
+        ///</summary>
+        [HttpGet]
+        public IQueryable<TripsHaveEmptySeatListItem> GetListOfTripHaveEmptySeatByStartDate(DateTime startDay)
+        {
+            ///<summary>
+            ///Select	s.Seat_Id, t.Trip_Id
+            ///From Bus b, Seat s, Trip t
+            ///Where    b.Bus_Id = s.Bus_Id and
+            ///         t.Bus_Id = b.Bus_Id and
+            ///         s.Status = 1
+            ///         EXCEPT
+            ///         Select sm.Seat_Id, t.Trip_Id
+            ///         From Trip t, Seat_Map sm
+            ///         Where t.Trip_Id = sm.Trip_Id and sm.Status = 1
+            ///	)       as subItem1st
+            ///</summary>
+
+            var subItem1st = from seat in db.Seats
+
+                             join bus in db.Bus on seat.Bus_Id equals bus.Bus_Id
+                             join trip in db.Trips on bus.Bus_Id equals trip.Bus_Id
+
+                             select new AvailableSeatByTripListItem
+                             {
+                                 Seat_Id = seat.Seat_Id,
+                                 Trip_Id = trip.Trip_Id,
+                                 Bus_Id = bus.Bus_Id,
+                             };
+
+            ///<summary>
+            ///Select Count(s.Seat_Id) as [Seat Trống], s.Trip_Id
+            ///From subItem1 
+            ///Group By s.Trip_Id as subItem2nd
+            ///</summary>
+            var subItem2nd = from items in subItem1st
+                             group items by items.Trip_Id into g
+                             select new { g.Key, g };
+
+            ///<summary>
+            ///
+            ///</summary>
+
+            var listItem = from trip in db.Trips
+
+                           join bus in db.Bus on trip.Bus_Id equals bus.Bus_Id
+                           join company in db.Companies on bus.Company_Id equals company.Company_Id
+                           join busType in db.Bus_Types on bus.Bus_Type_Id equals busType.Bus_Type_Id
+                           join route in db.Routes on trip.Route_Id equals route.Route_Id
+                           join items in subItem2nd on trip.Trip_Id equals items.Key
+
+                           where trip.Start_Date == startDay
+
+                           select new TripsHaveEmptySeatListItem
+                           {
+                               Trip_Id = trip.Trip_Id,
+                               Bus_Id = bus.Bus_Id,
+                               Bus_Type_Name = busType.Bus_Type_Name,
+                               CompanyName = company.Company_Name,
+                               Start_Point = route.Start_Point,
+                               Def_Point = route.Def_Point,
+                               Start_Date = trip.Start_Date,
+                               Start_Time = trip.Start_Time,
+                               Price = trip.Price,
+                               Empty_Seat_Sum = items.g.Select(x => x.Seat_Id).Count()
+                           };
+
+            return listItem;
+        }
+    }
+}
