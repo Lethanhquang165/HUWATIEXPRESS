f70d4384d443aff315873a60a3aef10cd3a13e55
Leo Andres <lethanhquang165@gmail.com>
2017-06-26 23:19:47 +0700

    Check in Route and CreateABill Controller

---
+++ HUWATIExpress/HUWATIExpress/Controllers/Mobile/CreateABillController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HUWATIExpress.Models;
+
+namespace HUWATIExpress.Controllers.Mobile
+{
+    public class CreateABillController : ApiController
+    {
+        huwadbDataContext db = new huwadbDataContext();
+
+        /// <summary>
+        /// GET EMPTY FORM FOR ADDING CUSTOMER
+        /// </summary>
+
+        [HttpGet]
+        public void AddNewCustomer()
+        {
+            var customer = new Customer();
+        }
+
+        /// <summary>
+        /// POST VALUE TO ADD A CUSTOMER BY SQL QUERY
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="address"></param>
+        /// <param name="phone"></param>
+        /// <param name="status"></param>
+        /// <param name="idnum"></param>
+        /// <param name="accid"></param>
+        /// 
+        [AcceptVerbs("GET")]
+        [HttpPost]
+        public void AddNewCustomer(string name, string address, decimal phone, bool status, decimal idnum, int accid)
+        {
+            var customer = new Customer
+            {
+                Customer_Name = name,
+                Address = address,
+                Phone = phone,
+                Status = status,
+                Identication_Number = idnum,
+                Account_Id = accid,
+            };
+            db.Customers.InsertOnSubmit(customer);
+            db.SubmitChanges();
+        }
+
+        [HttpGet]
+        public void AddNewAccount()
+        {
+            var account = new Account();
+        }
+
+        [AcceptVerbs("Get")]
+        [HttpPost]
+        public void AddNewAccount(string username, string password, int roleid)
+        {
+            var account = new Account
+            {
+                User_Name = username,
+                Password = password,
+                Role_Id = roleid
+            };
+            db.Accounts.InsertOnSubmit(account);
+            db.SubmitChanges();
+        }
+    }
+}
---
+++ HUWATIExpress/HUWATIExpress/Controllers/Mobile/RouteController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HUWATIExpress.Models;
+
+namespace HUWATIExpress.Controllers.Mobile
+{
+    public class RouteController : ApiController
+    {
+        huwadbDataContext db = new huwadbDataContext();
+        ///<summary>
+        /// GET LIST OF START POINT
+        /// </summary>
+        
+        public IQueryable<string> GetListOfStartPoints()
+        {
+            var listItem = from route in db.Routes select route.Start_Point;
+            return listItem;
+        }
+
+        ///<summary>
+        /// GET LIST OF DEFINITION POINT
+        /// </summary>
+
+        public IQueryable<string> GetListOfDefPoints()
+        {
+            var listItem = from route in db.Routes select route.Def_Point;
+            return listItem;
+        }
+
+        ///<summary>
+        /// GET LIST OF ROUTES
+        /// </summary>
+
+        public IQueryable<Route> GetListOfRoutes()
+        {
+            var listItem = from route in db.Routes select route;
+            return listItem;
+        }
+    }
+}
